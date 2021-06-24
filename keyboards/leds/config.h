f0eff7ca435552134d8e092a6e867b68a886293a
Lei Yu <yuleiz@gmail.com>
2021-06-24 22:15:19 +0800

    added leds

---
+++ keyboards/leds/config.h
@@ -0,0 +1,53 @@
+/**
+ * config.h
+ *
+  Copyright 2020 astro <yuleiz@gmail.com>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x60BE
+#define PRODUCT_ID      0x0101
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    astro
+#define PRODUCT         LEDS
+
+/* key matrix size */
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 4
+
+#define DIRECT_PINS { { E6, B3, B1, B2} }
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+//rgb light setting
+#define RGBLED_NUM      16
+#define RGB_DI_PIN      C6
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
