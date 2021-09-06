ca9650778773b90cd9f86928ed63dbef761191a6
yulei <yuleiz@gmail.com>
2021-09-06 22:16:51 +0800

    update vial json

--- keyboards/inett_studio/n6/config.h
+++ keyboards/inett_studio/n6/config.h
@@ -21,8 +21,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x694E  //'iN'
-#define PRODUCT_ID      0x4E36  //'N6'
+#define VENDOR_ID       0x4E65   //'Ne'
+#define PRODUCT_ID      0x4E36   //'N6'
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Neson Design
 #define PRODUCT         N6 Keyboard
